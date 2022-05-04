96a59defc456d7ef9fa7601741a8ce2ff1fedd7b
AI-Pree <sexypistolcs@gmail.com>
2022-05-04 23:05:15 +1000

    change the title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: "HGEN FINANCE",
+        title: "HGEN Finance",
         htmlAttrs: {
             lang: "en",
         },
